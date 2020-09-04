cb53867930282ff3c01d61ad6d91129fa9ec5c7f
bannafi <54008404+bannafi@users.noreply.github.com>
2020-09-04 14:27:14 +0700

    yang bener

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+    <div className='formLabel'>
+    <h1>Form Pembelian Buah</h1>
+    
+    <form action="">
+        <table>
+            <tr>
+                <th><label for="Nama">Nama Pelanggan</label></th>
+                <td><input type="text" id="nama" /></td>
+            </tr>
+           
+            <tr>
+                <th id='daftarItem'><label >Daftar Item</label></th>
+                <td>
+                    <input type="checkbox" name="item" value="semangka" />Semangka <br />
+                    <input type="checkbox" name="item" value="jeruk" />Jeruk <br />
+                    <input type="checkbox" name="item" value="nanas" />Nanas <br />
+                    <input type="checkbox" name="item" value="salak" />Salak <br />
+                    <input type="checkbox" name="item" value="anggur" />Anggur <br />
+                </td>
+            </tr>
+        </table>
+        <input type="submit" value="Kirim" className='button' />
+    </form> <br />
+  </div>
+    <a href="index.html">Kembali Ke Index</a>
+</body>
+    </div>
+  );
+}
+
+export default App;
